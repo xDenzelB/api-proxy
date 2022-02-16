56535acd9017adb93e21dbe33cbb353b4c7d5cec
Denzel <dbartolaba123@gmail.com>
2022-02-15 16:25:13 -0800

    Started yelp backend

--- functions/yelp.js
+++ functions/yelp.js
@@ -1,21 +1,29 @@
-const request = require('superagent');
+const fetch = require('node-fetch');
 
 require('dotenv').config();
 
+
 exports.handler = async (event, context) => {
   try {
-    // grab the city, state, and country from the request's query parameters
+    const name = event.queryStringParameters.search;
+
+
+    const endPointURL = `https://api.yelp.com/v3/businesses/search?location=${name}`;
+
+    const response = await fetch(endPointURL);
+
+    const json = await response.json();
+    // grab the pokemon's name from the request's query parameters
     // here is an example from the netlify docs:
     // https://functions.netlify.com/playground/#hello%2C-%7Bname%7D 
+    console.log(json);
+    // consult the pokedex docs 
+    // https://pokedex-alchemy.herokuapp.com/
 
-    // consult the yelp docs to figure out how to use a city, state, and country to make a request for businesses
-    // https://www.yelp.com/developers/documentation/v3/business_search
-    // don't forget to add the yelp API key!
-    
     return { 
       statusCode: 200, 
-    // this is where you shoot data back to the user. right now it's sending an empty object--replace this with the yelp data. remember, you do need to stringify it, otherwise netlify gets mad. ¯\_(ツ)_/¯
-      body: JSON.stringify({}),
+    // this is where you shoot data back to the user. right now it's sending an empty object--replace this with the pokemon data. remember, you do need to stringify it, otherwise netlify gets mad. ¯\_(ツ)_/¯
+      body: JSON.stringify(json),
     };
   } catch (error) {
     console.log(error);
@@ -25,3 +33,4 @@ exports.handler = async (event, context) => {
     };
   }
 };
+
