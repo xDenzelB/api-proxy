1b02d001b9187792352d9794e7c909127c928a5c
Denzel <dbartolaba123@gmail.com>
2022-02-15 17:16:29 -0800

    Yelp search works now

--- functions/yelp.js
+++ functions/yelp.js
@@ -3,26 +3,24 @@ const fetch = require('node-fetch');
 require('dotenv').config();
 
 
-exports.handler = async (event, context) => {
+exports.handler = async (event) => {
   try {
     const name = event.queryStringParameters.search;
 
 
-    const endPointURL = `https://api.yelp.com/v3/businesses/search?location=${name}`;
-
-    const response = await fetch(endPointURL);
+    const response = await fetch (`https://api.yelp.com/v3/businesses/search?location=${name}`, {
+      headers : {
+        Authorization: `Bearer ${process.env.YELP_KEY}`
+      }
+    });
 
     const json = await response.json();
-    // grab the pokemon's name from the request's query parameters
-    // here is an example from the netlify docs:
-    // https://functions.netlify.com/playground/#hello%2C-%7Bname%7D 
+
     console.log(json);
-    // consult the pokedex docs 
-    // https://pokedex-alchemy.herokuapp.com/
+   
 
     return { 
       statusCode: 200, 
-    // this is where you shoot data back to the user. right now it's sending an empty object--replace this with the pokemon data. remember, you do need to stringify it, otherwise netlify gets mad. ¯\_(ツ)_/¯
       body: JSON.stringify(json),
     };
   } catch (error) {
--- src/YelpSearch.js
+++ src/YelpSearch.js
@@ -3,7 +3,7 @@ import YelpList from './YelpList';
 import Loading from './load.gif';
 
 export default function YelpSearch() {
-  const [search, setSearch] = useState('Pikachu');
+  const [search, setSearch] = useState('Portland, OR');
   const [business, setBusiness] = useState([]);
   const [loading, setLoading] = useState(false);
       // you'll need to track your pokemon search results, the loading state, and one form field: name. For this form field, set a real initial values (like 'pikachu') so the form populates with a default value.
@@ -17,7 +17,7 @@ export default function YelpSearch() {
 
       const json = await response.json();
 
-      setBusiness(json);
+      setBusiness(json.businesses);
       setLoading(false);
     } catch (e) {
       console.log('=============================\n');
@@ -33,19 +33,19 @@ export default function YelpSearch() {
 
       
   return (
-    <section className='pokemon'>
+    <section className='yelp'>
       {/* make the fetch on submit */}
       <form onSubmit={handleYelpSubmit}>
         <input onChange={e => setSearch(e.target.value)} value={search} />
-            Search pokemon for a city
+            Search businesses 
         {/* add inputs/labels for city name, state, and country, using all the things we need with react forms. Don't forget to use the value property to sync these up with the default values in react state */}
-        <button>Get pokemon</button>
+        <button>Get Business</button>
       </form>
 
       <div>
         {
           loading ? <img src={Loading} />  
-            : <YelpList business={business}/>
+            : <YelpList business={business} />
         }
       </div>
       {/* Make a PokemonList component to import and use here. Use a ternery to display a loading spinner (make a <Spinner /> component for this) if the data is still loading. */}
