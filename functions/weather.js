c49728d64e6f592ac443346ad29f95b62912079d
Denzel <dbartolaba123@gmail.com>
2022-02-16 09:45:48 -0800

    Have the lat/lon and geo locationin back end

--- functions/weather.js
+++ functions/weather.js
@@ -5,9 +5,15 @@ require('dotenv').config();
 exports.handler = async (event) => {
   try {
 
-    const name = event.queryStringParameters.search;
+    const { city, state, country } = event.queryStringParameters;
 
-    const response = await fetch (`https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=${process.env.WEATHER_KEY}`)
+    const geoLocationResponse = await fetch (`http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${country}&limit={limit}&appid=${process.env.WEATHER_KEY}`); 
+
+    const [{ lat, lon }] = await geoLocationResponse.json();
+
+    const response = await fetch (`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${process.env.WEATHER_KEY}`);
+
+    const weatherJson = await response.json();
     // grab the city, state, and country from the request's query parameters
     // here is an example from the netlify docs:
     // https://functions.netlify.com/playground/#hello%2C-%7Bname%7D 
@@ -23,7 +29,7 @@ exports.handler = async (event) => {
     return { 
       statusCode: 200, 
     // this is where you shoot data back to the user. right now it's sending an empty object--replace this with the weather data. remember, you do need to stringify it, otherwise netlify gets mad. ¯\_(ツ)_/¯
-      body: JSON.stringify({}),
+      body: JSON.stringify(weatherJson),
     };
   } catch (error) {
     console.log(error);
